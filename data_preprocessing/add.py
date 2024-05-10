200e2927159a8bfb37c4ce0b8609d6db498dc566
dlgksqls <horainlee87@gmail.com>
2024-05-10 23:01:25 +0900

    exception fix

--- data_preprocessing/add.py
+++ data_preprocessing/add.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import chardet
 
 def fix_encoding(file_path):
@@ -13,19 +12,3 @@ csv_file_path = "C:\Langchain\poat\langchain\대구광역시_관광지_완.csv"
 
 # 인코딩 수정
 fix_encoding(csv_file_path)
-=======
-import chardet
-
-def fix_encoding(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        content = f.read()
-
-    with open(file_path, 'w', encoding='cp949') as f:
-        f.write(content)
-
-# 파일 경로
-csv_file_path = "C:\Langchain\poat\langchain\대구광역시_관광지_완.csv"
-
-# 인코딩 수정
-fix_encoding(csv_file_path)
->>>>>>> d5b88bdf0 (Test)
