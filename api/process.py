07a023ab169f0fd1e84137307ea2484fa9f426f5
dlgksqls <horainlee87@gmail.com>
2024-05-24 14:11:10 +0900

    place_photo add

--- api/process.py
+++ api/process.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import django
+from django.core.files import File
 
 # Django 설정을 로드
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -8,17 +9,16 @@ django.setup()
 
 from places.models import Place, Tag
 
-# CSV
-# df = pd.read_csv(r"/Users/leehb/Desktop/BE_AI_GO/api/dataset.csv", encoding='cp949')
-df = pd.read_csv(r"/srv/BE_AI_GO/api/dataset.csv", encoding='cp949')
+# CSV 파일 로드
+df = pd.read_csv(r"dataset.csv", encoding='cp949')
 
 for index, row in df.iterrows():
     # 공백 기준으로 list로 불러오기
     tag_list = list(str(row["tag"]).split())
-    
+
     # 주차 여부 bool로 넣기
     parking_bool = (row["parking"] == "유")
-    
+
     if row["hardness"] == '-':
         save_hardness = 0.0
     else:
@@ -29,10 +29,26 @@ for index, row in df.iterrows():
     else:
         save_latitude = row["latitude"]
 
+    if "image_name" in row:
+        image_name = row["image_name"]
+    else:
+        image_name = f'{row["name"]}.jpg'
+
+    # 이미지 파일 경로
+    IMAGE_DIR = 'media/이미지'
+
+    image_path = os.path.join(IMAGE_DIR, image_name)
+
+    # 이미지 파일 존재 여부 확인 및 File 객체 생성
+    if os.path.exists(image_path):
+        image_file = File(open(image_path, 'rb'), name=image_name)
+    else:
+        image_file = None
+
     # db에 넣기
     place = Place.objects.create(
         name=row["name"],
-        image=None,
+        image=image_file,
         classification=row["classification"],
         street_name_address=row["street_name_adress"],
         parking=parking_bool,
@@ -42,10 +58,14 @@ for index, row in df.iterrows():
         latitude=save_latitude,
         time=row["time"]
     )
-    
+
     # 태그 추가
     for tag_name in tag_list:
         tag, _ = Tag.objects.get_or_create(name=tag_name)
         place.tag.add(tag)
 
-    place.save()  # 장소를 저장합니다.
+    # 이미지 파일이 열려 있으면 닫기
+    if image_file:
+        image_file.close()
+
+    place.save()  # 장소를 저장
